c19c0b45e62cae7e380c9f157d8610d07643ad5e
Jugal Panchal <jugalpanchal@gmail.com>
2017-03-09 08:54:01 +0530

    Remove duplicate file

--- smartwatersystem/mqtt_work/mqtt_work.ino
+++ smartwatersystem/mqtt_work/mqtt_work.ino
@@ -7,6 +7,12 @@ ESP esp(&Serial, &debugPort, 4);
 MQTT mqtt(&esp);
 boolean wifiConnected = false;
 
+/* Ultrasonic Sensor */
+int trigPin = 12;
+int echoPin = 11;
+//
+
+
 void wifiCb(void* response)
 {
   uint32_t status;
@@ -65,6 +71,11 @@ void mqttPublished(void* response)
 
 }
 void setup() {
+
+  /* Ultrasonic Sensor */
+  //pinMode(trigPin, OUTPUT);
+  //pinMode(echoPin, INPUT); 
+  
   Serial.begin(19200);
   debugPort.begin(19200);
   esp.enable();
@@ -93,7 +104,7 @@ void setup() {
   debugPort.println("ARDUINO: setup wifi");
   esp.wifiCb.attach(&wifiCb);
 
-  esp.wifiConnect("iot","jugal1234");
+  esp.wifiConnect("LGMobile","jugal1234");
 
 
   debugPort.println("ARDUINO: system started");
@@ -102,6 +113,51 @@ void setup() {
 void loop() {
   esp.process();
   if(wifiConnected) {
+    //char tmp[255];
+    long distance = getDistance();
+    //sprintf(tmp, "%ld", distance);
 
+    //String thisString = String(distance);
+
+    char temp[10];
+    ltoa(9L
+    
+    ,temp,10);
+    
+    mqtt.publish("/sws/test2",temp);
   }
+}    
+
+
+long getDistance()
+{
+  long t = 0, h = 0, hp = 0;
+  
+  // Transmitting pulse
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Waiting for pulse
+  t = pulseIn(echoPin, HIGH);// t is length of pulse in microsecond
+  //works on pulses from 10 microsecond to 3 min length
+  
+  // Calculating distance 
+  h = t / 58;//h is hight in CM //if need hight in inch then divide by 148
+  
+  //h = h - 6;  // offset correction
+  //h = 50 - h;  // water height, 0 - 50 cm
+  //hp = 2 * h;  // distance in %, 0-100 %
+
+  
+  // Sending to computer
+  Serial.print(h);
+  //Serial.print(hp);
+  // Serial.print(" cm\n");
+  Serial.print("\n");
+  
+  delay(1000);
+  return h;
 }
